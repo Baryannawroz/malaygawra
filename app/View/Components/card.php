2a4c3b63b0fffa97d2377f75607f3b7d8929ef0a
baryannawroz <baryannawroz1@gmail.com>
2024-08-10 14:41:23 +0300

    add cards to the dashboreds

---
+++ app/View/Components/card.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class card extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return <<<'blade'
+ <div
+            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+                acquisitions
+                2021</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
+                acquisitions of
+                2021 so far, in reverse chronological order.</p>
+        </div>
+blade;
+    }
+}
---
+++ resources/views/components/cards.blade.php
@@ -0,0 +1,7 @@
+<div class="flex items-center justify-center min-h-screen">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+        <x-card>
+        </x-card>
+    </div>
+</div>
