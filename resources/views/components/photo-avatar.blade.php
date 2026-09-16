1b4524bdb0edd7e31593c936ff9716aac834020b
Baryannawroz <89686793+Baryannawroz@users.noreply.github.com>
2026-09-17 02:48:13 +0300

    Refactor photo avatar component to improve photo path handling and ensure proper rendering of images based on availability.

--- resources/views/components/photo-avatar.blade.php
+++ resources/views/components/photo-avatar.blade.php
@@ -1,9 +1,17 @@
 @props(['path' => null, 'name' => '', 'size' => null])
 
 @php
-    $photo = $path ? ltrim(str_replace('\\', '/', $path), '/');
-    $photo = preg_replace('#^(storage/|public/)+#', '', $photo);
+    $photo = $path ? ltrim(str_replace('\\', '/', $path), '/') : '';
+    $photo = $photo ? preg_replace('#^(storage/|public/)+#', '', $photo) : '';
 @endphp
+
+<div {{ $attributes->merge(['class' => 'mg-avatar' . ($size === 'xl' ? ' mg-avatar-xl' : '')]) }}>
+    @if ($photo)
+    <img src="{{ url('storage/' . $photo) }}" alt="{{ $name }}"
+        loading="lazy" onerror="this.remove()">
+    @endif
+    <span>{{ mb_substr((string) $name, 0, 1) }}</span>
+</div>
 
 <div {{ $attributes->merge(['class' => 'mg-avatar' . ($size === 'xl' ? ' mg-avatar-xl' : '')]) }}>
     @if ($photo)
